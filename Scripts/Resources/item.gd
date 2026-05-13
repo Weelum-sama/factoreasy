f547cbf007c914df54b0db3ce4f54b6b48dd2c15
Dylan Vermeulen <dylan.vermeulen@hva.nl>
2026-05-13 23:21:43 +0200

    setup for grid

--- Scripts/Resources/item.gd
+++ Scripts/Resources/item.gd
@@ -1,4 +1,9 @@
 extends Resource
 class_name Item
 
-@export var id: int
+@export var name: String
+@export var texture: Texture2D
+@export var sell_value: float
+
+func _to_string() -> String:
+	return name

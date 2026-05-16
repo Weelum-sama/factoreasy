3c1d02e219764f219444b8698bc0c4799c3d5899
Weelum-sama <dylan.vermeulen@hva.nl>
2026-05-16 17:19:52 +0200

    basic camera movement

---
+++ Scripts/camera_script.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+const MIN_ZOOM: Vector2 = Vector2(1.0, 1.0)
+const MAX_ZOOM: Vector2 = Vector2(0.1, 0.1)
+const ZOOM_INCREMENT: Vector2 = Vector2(0.02, 0.02)
+const MOVE_SPEED: float = 15
+
+var movement_vector: Vector2
+
+func _process(delta: float) -> void:
+	_control_zoom()
+	_control_position()
+	_control_rotation()
+
+func _control_zoom() -> void:
+	if Input.is_action_just_pressed("Zoom In"):
+		zoom = clamp(zoom + ZOOM_INCREMENT, MAX_ZOOM, MIN_ZOOM)
+	elif Input.is_action_just_pressed("Zoom Out"):
+		zoom = clamp(zoom - ZOOM_INCREMENT, MAX_ZOOM, MIN_ZOOM)
+
+func _control_position() -> void:
+	var input_direction = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
+	position += input_direction * MOVE_SPEED
+
+func _control_rotation() -> void:
+	if Input.is_action_just_pressed("Rotate"):
+		rotate(PI/2.0)

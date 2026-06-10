27618d64d5eed4cedd3ec0bdd35a5a4e1b3788bd
Weelum-sama <dylan.vermeulen@hva.nl>
2026-06-10 13:06:46 +0200

    small fixes

--- Objects/Scripts/UI/store_tab.gd
+++ Objects/Scripts/UI/store_tab.gd
@@ -34,6 +34,7 @@ func _add_entry(data: OreNodeData) -> void:
 	_buttons[data.id] = button
 	button.data.update_purchase_cost()
 	button.update_label_cost(button.data.cost)
+	button.update_affordability(GameState.get_total_coins() >= button.data.cost)
 
 func _on_coins_changed(new_amount: float) -> void:
 	for node_id in _buttons:
@@ -43,8 +44,7 @@ func _on_coins_changed(new_amount: float) -> void:
 func _on_node_unlocked(_id: String) -> void:
 	_refresh()
 
-@warning_ignore("unused_parameter")
-func _on_purchase(node_id: String, amount: int) -> void:
+func _on_purchase(node_id: String, _amount: int) -> void:
 	_buttons[node_id].data.update_purchase_cost()
 	_buttons[node_id].update_label_owned(GameState.total_nodes_owned[node_id])
 	_buttons[node_id].update_label_cost(_buttons[node_id].data.cost)
